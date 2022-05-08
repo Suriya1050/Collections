bde10d64b3e9ef27105fbfd92ac2540fa2e0acd5
Suriya1050 <soorya0593@gmail.com>
2022-05-08 20:09:37 +0530

    Sowmiya

--- src/org/sample/GitTesting.java
+++ src/org/sample/GitTesting.java
@@ -5,6 +5,11 @@ public class GitTesting {
 	private void empId() {
 		System.out.println("Hi");
 	}
+	
+	private void phone() {
+		// TODO Auto-generated method stub
+
+	}
 	public static void main(String[] args) {
 		
 		GitTesting GT = new GitTesting();
